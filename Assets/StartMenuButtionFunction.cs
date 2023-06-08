a5b725f207815c7f13ed754e254598ac1f31506b
rrreol <chocomana0416@gmail.com>
2023-06-09 00:19:23 +0800

    client can join, but can't move correctly

--- Assets/StartMenuButtionFunction.cs
+++ Assets/StartMenuButtionFunction.cs
@@ -14,6 +14,7 @@ public class StartMenuButtionFunction : MonoBehaviour
 
     public void JoinGameButtonFunction()
     {
-
+        NetworkManager.Singleton.StartClient();
+        NetworkManager.Singleton.SceneManager.LoadScene("GameplayScene", LoadSceneMode.Single);
     }
 }
