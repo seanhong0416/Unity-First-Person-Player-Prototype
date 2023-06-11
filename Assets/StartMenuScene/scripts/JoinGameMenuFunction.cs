b23e9a7539ea1552f9a26a1023a2c40bd5fa5aa4
rrreol <chocomana0416@gmail.com>
2023-06-11 19:30:32 +0800

    multiplayer UI done

--- Assets/StartMenuScene/scripts/JoinGameMenuFunction.cs
+++ Assets/StartMenuScene/scripts/JoinGameMenuFunction.cs
@@ -10,8 +10,15 @@ using System;
 public class JoinGameMenuFunction : NetworkBehaviour
 {
     [SerializeField] GameObject inputField;
+    
+    //text
     [SerializeField] GameObject connectingSitutation;
+    [SerializeField] GameObject timeoutCountdown;
+
+    //menu or page
     [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject connectingPage;
+    [SerializeField] GameObject notConnectingPage;
 
     float time_stamp = 0f;
     bool connection_timeout_counting_flag = false;
@@ -20,11 +27,12 @@ public class JoinGameMenuFunction : NetworkBehaviour
     {
         if (connection_timeout_counting_flag)
         {
-            Debug.Log(Time.time - time_stamp);
-            if(Time.time - time_stamp > 10)
+            //Debug.Log(Time.time - time_stamp);
+            timeoutCountdown.GetComponent<TMP_Text>().SetText("Connecting...\nTimeout:" + ( 10 - (int)(Time.time-time_stamp) ) );
+
+            if (Time.time - time_stamp > 10)
             {
-                NetworkManager.Singleton.Shutdown();
-                connection_timeout_counting_flag = false;
+                ConnectionFailed();
             }
         }
     }
@@ -42,6 +50,8 @@ public class JoinGameMenuFunction : NetworkBehaviour
             Debug.Log("Set Timestamp");
             time_stamp = Time.time;
             connection_timeout_counting_flag = true;
+            connectingPage.SetActive(true);
+            notConnectingPage.SetActive(false);
         }
     }
 
@@ -50,4 +60,15 @@ public class JoinGameMenuFunction : NetworkBehaviour
         mainMenu.SetActive(true);
         gameObject.SetActive(false);
     }
+
+    public void ConnectionFailed()
+    {
+        NetworkManager.Singleton.Shutdown();
+        notConnectingPage.SetActive(true);
+        connectingSitutation.GetComponent<TMP_Text>().SetText("Server Connection Timeout");
+        connectingPage.SetActive(false);
+        connection_timeout_counting_flag = false;
+    }
+
+
 }
